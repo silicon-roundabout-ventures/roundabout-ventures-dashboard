7830c0306a02459f02bea8757aa786bb916c02ce
Francesco Perticarari <23483149+fpert041@users.noreply.github.com>
2025-11-20 21:55:11 +0000

    Update SubstackFeed.tsx to match script updates

--- src/components/widgets/SubstackFeed.tsx
+++ src/components/widgets/SubstackFeed.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SubstackFeed: React.FC = () => (
-  <div id="substack-feed-embed" className="h-[600px] overflow-auto mb-8" />
+  <div data-supascribe-embed-id="577342797081" id="substack-feed-embed" className="h-[600px] overflow-auto mb-8" />
 );
 
 export default SubstackFeed;
